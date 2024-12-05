a164ec36449fa9f7a6c65a645126685ca27938ef
Faizan shaik <138827042+faizan736@users.noreply.github.com>
2024-12-06 01:15:45 +0530

    Create day1.java

---
+++ day1/day1.java
@@ -0,0 +1,14 @@
+class day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    //Jdk = (JRE(JVM+libraries)+development tools)
+    //c++ is compiled into machine code
+    // java is converted in byte code which makes this language portable
+    //c++ has new ,delete and pointers
+    //java automatic memory mangement 
+
+    //--------------------------------------------
+    
+
+}
